d40954f521639ad8d33dea89522f9c7252c9f552
Zein Khalifeh <Zein.khalifeh@gmail.com>
2015-07-04 22:13:23 +0300

    User sign up WIP

--- spec/features/user_sign_ups_spec.rb
+++ spec/features/user_sign_ups_spec.rb
@@ -1,13 +1,24 @@
 require 'rails_helper'
 
 feature "User signs up", type: :feature do
+  # before do
+  #   click_link "Sign up"
+  #   expect(current_path).to eq(new_user_registration_path)
+  #   fill_in "Name", with: "Test"
+  #   fill_in "Surname", with: "Test"
+  #   fill_in "Email", with: "tester@example.tld"
+  #   fill_in "Password", with: "test-password"
+  #   fill_in "Password confirmation", with: "test-password"
+  #   click_button "Sign up"
+  # end
+
   scenario "successfully" do
     visit "/"
 
     click_link "Sign up"
     expect(current_path).to eq(new_user_registration_path)
-    fill_in "Name", with: "Zein"
-    fill_in "Surname", with: "Khalifeh"
+    fill_in "Name", with: "Test"
+    fill_in "Surname", with: "Test"
     fill_in "Email", with: "tester@example.tld"
     fill_in "Password", with: "test-password"
     fill_in "Password confirmation", with: "test-password"
@@ -31,7 +42,71 @@ feature "User signs up", type: :feature do
 
     expect(current_path).to eq "/"
     expect(page).to have_content "Signed in successfully."
-    expect(page).to have_content "Hello Zein"
+    expect(page).to have_content "Hello Test"
+
+
+  end
+
+  scenario "usuccessfully - Invalid email " do
+    visit "/"
+
+    click_link  "Sign up"
+    expect(current_path).to eq(new_user_registration_path)
+    fill_in "Name", with: "Test"
+    fill_in "Surname", with: "Test"
+    fill_in "Email", with: "invalid-email"
+    fill_in "Password", with: "test-password"
+    fill_in "Password confirmation", with: "test-password"
+    click_button "Sign up"
+    expect(current_path).to eq "/users"
+    expect(page).to have_content(
+      "Email is invalid"
+        )
+  end
+  scenario "usuccessfully - Duplicate email " do
+    visit "/"
+
+    click_link "Sign up"
+    expect(current_path).to eq(new_user_registration_path)
+    fill_in "Name", with: "Test"
+    fill_in "Surname", with: "Test"
+    fill_in "Email", with: "tester@example.tld"
+    fill_in "Password", with: "test-password"
+    fill_in "Password confirmation", with: "test-password"
+    click_button "Sign up"
+    expect(current_path).to eq "/users"
+    expect(page).to have_content(
+      "Email has already been taken"
+        )
+  end
+
+end
+
+feature "User signing", type: :feature do
+  scenario "usuccessfully" do
+    visit "/"
+
+    click_link "Sign in"
+    expect(current_path).to eq(new_user_session_path)
+    fill_in "Email", with: "wrong-email@example.tld"
+    fill_in "Password", with: "wrong-password"
+    click_button "Log in"
+
+    expect(current_path).to eq(new_user_session_path)
+    expect(page).to have_content(
+      "Invalid email or password."
+    )
+
+    # expect(current_path).to eq new_user_session_path
+    # expect(page).to have_content "Your email address has been successfully confirmed."
+
+    # fill_in "Email", with: "tester@example.tld"
+    # fill_in "Password", with: "test-password"
+    # click_button "Log in"
+
+    # expect(current_path).to eq "/"
+    # expect(page).to have_content "Signed in successfully."
+    # expect(page).to have_content "Hello Zein"
 
 
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,4 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+  #Does this require a Gem? 
+  #it { should validate_uniqueness_of(:email) }
+
 end
